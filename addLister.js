59611b417e7ad03d8f4a765f08d8df04f6642229
koringz <260713986@qq.com>
2016-05-14 23:38:37 +0800

    update

--- addLister.js
+++ addLister.js
@@ -1,23 +1,29 @@
 !function(w){
-
+	var koringz = {};
 	/**********调用**********/
-
-	function el(element){
-		if(!element){element = document}
-		return element 
-	};
-
-	function addLister(domnode,eventType,handler){
-		if(el().all){
-			domnode.attachEvent("on"+eventType,handler)
-		}else{
-			domnode.addEventListener(eventType,handler,false)
-		}
-	};
+	  koringz.el = function (element){
+	    	if(!element){element = document}
+	    	return element 
+	  };
+	  
+	  koringz.hookAddLister = function (el,eventType,handler,isbool){
+	    	isbool = isbool || false;
+	    	if(el.addEventListener){
+	    		el.addEventListener(eventType,handler,isbool);
+	        }
+	  };
+	  
+	  koringz.addLister = function (domnode,eventType,handler,isbool){
+	    	if(koringz.el().all){
+	      		domnode.attachEvent("on"+eventType,handler)
+	    	}
+	    	koringz.hookAddLister(domnode,eventType,handler,isbool);
+	  };
 
 	/*********调用***********/
-
-	w.addLister = addLister(your_node,Event[0],function_name);
+	
+	w.addLst = koringz.addLister;
 
 	// var Event = ["click","drag","moveover","moveout","keyup",.....]
+	// addLst(your_node,Event[0],function_name,isbo);
 }(window);
